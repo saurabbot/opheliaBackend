9080c699cd87a62b0dcd7d6f3944ce4995b8ca1c
firefox292 <saurabh.n@nestaway.com>
2023-05-23 01:36:28 +0530

    added a cookie return  to the login route

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { z } from 'zod'
 import { db } from '../lib/prisma'
 import bcrypt from 'bcrypt'
+import { v4 as uuidv4 } from 'uuid'
 const signUpSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
@@ -30,6 +31,7 @@ export const signUp = async (req: Request, res: Response) => {
     res
       .status(201)
       .json({ message: 'User created successfully', user: newUser })
+      .end()
   } catch (err) {
     console.error(err)
     res.status(500).json({ error: 'Internal Server Error' })
@@ -46,9 +48,31 @@ export const signIn = async (req: Request, res: Response) => {
       res.status(400).json({ message: 'please enter all details' })
     }
     const user = await db.user.findFirst({ where: { email } })
+    const isPasswordSame = await bcrypt.compare(password, user.password)
+    if (isPasswordSame) {
+      // req.session.user = user
 
+      const sessionId = uuidv4()
+
+      res.cookie('sessionId', sessionId, {
+        httpOnly: true,
+        secure: true, // Enable if using HTTPS
+        sameSite: 'strict',
+        maxAge: 30 * 24 * 60 * 60 * 1000 // Set the expiration time for the cookie (30 days)
+      })
+      res.status(200).json({ result: true, message: 'Logged in', user })
+      return
+    } else {
+      res
+        .status(403)
+        .json({ result: false, message: 'authentication failed', user })
+    }
   } catch (err) {
     console.error(err)
     res.status(500).json({ error: 'Internal server error' })
   }
 }
+
+// export const isUserLoggedIn = (req: Request, res: Response) => {
+
+// }
