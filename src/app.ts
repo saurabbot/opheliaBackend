8e5b253a6ca25672960c1b7b46556dabd1e8826b
firefox292 <saurabh.n@nestaway.com>
2023-05-28 19:00:13 +0530

    session working fine now and go ahead and build the user dashboard

--- src/app.ts
+++ src/app.ts
@@ -2,30 +2,42 @@ import express from 'express'
 import { Express } from 'express'
 import router from './router'
 import session from 'express-session'
-import http from 'http'
+// import http from 'http'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import { setupSwagger } from './swagger'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import { sessionsHandler } from './controller/auth'
+// import { setupSwagger } from './swagger'
 
 const app: Express = express()
 const port = 4000
 app.use(
   cors({
-    credentials: true
+    origin: 'http://localhost:3000', // Replace with your UI domain
+    credentials: true, // Enable sending cookies,
+    preflightContinue: true
   })
 )
 
 // app.use(compression())
+morgan('tiny')
+app.use(helmet())
 app.use(cookieParser())
 app.use(bodyParser.json())
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-setupSwagger(app)
+// app.get('/auth/sessions', sessionsHandler)
+app.use('/', router())
+
+// app.get('/auth/sessions', (req, res) => {
+//   console.log(req.cookies) // Log the value of req.cookies
+//   const cookies = req.cookies
+//   res.cookie('sessionId', '3c196d67-b096-49f1-874c-19b32c25bc45')
+//   res.send(`Cookie set successfully${JSON.stringify(cookies)}`)
+// })
+// setupSwagger(app)
 // const server = http.createServer(app)
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
-app.use('/', router())
--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { z } from 'zod'
 import { db } from '../lib/prisma'
 import bcrypt from 'bcrypt'
+// import cookieParser from 'cookie-parser'
 import { v4 as uuidv4 } from 'uuid'
 const signUpSchema = z.object({
   email: z.string().email(),
@@ -13,7 +14,7 @@ export const signUp = async (req: Request, res: Response) => {
   try {
     const { email, password, username } = signUpSchema.parse(req.body)
     if (!email || !username || !password) {
-      res.status(400).json({ message: 'please enter all details' })
+      return res.status(400).json({ message: 'please enter all details' })
     }
     const existingUser = await db.user.findFirst({ where: { email } })
     if (existingUser) {
@@ -37,7 +38,7 @@ export const signUp = async (req: Request, res: Response) => {
         sessions: true
       }
     })
-    res
+    return res
       .status(201)
       .json({
         result: true,
@@ -58,18 +59,17 @@ export const signIn = async (req: Request, res: Response) => {
   try {
     const { email, password } = singInSchema.parse(req.body)
     if (!email || !password) {
-      res.status(400).json({ message: 'please enter all details' })
+      return res.status(400).json({ message: 'please enter all details' })
     }
     const user = await db.user.findFirst({ where: { email } })
-    const isPasswordSame = await bcrypt.compare(password, user.password)
     if (!user) {
-      res.status(404).json({
-        resullt: false,
+      return res.status(404).json({
+        result: false,
         message: 'User not found, check email once again'
       })
     }
+    const isPasswordSame = await bcrypt.compare(password, user.password)
     if (isPasswordSame) {
-      // req.session.user = user
       const sessionToken = uuidv4()
       const expires = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
       const session = await db.session.create({
@@ -85,35 +85,64 @@ export const signIn = async (req: Request, res: Response) => {
         sameSite: 'strict',
         maxAge: 30 * 24 * 60 * 60 * 1000 // Set the expiration time for the cookie (30 days)
       })
-      res.status(200).json({ result: true, message: 'Logged in', user })
-      return
+      return res.status(200).json({ result: true, message: 'Logged in', user })
     } else {
-      res.status(403).json({ result: false, message: 'Authentication failed' })
+      return res
+        .status(403)
+        .json({ result: false, message: 'Authentication failed' })
     }
   } catch (err) {
     console.error(err)
-    res.status(500).json({ error: 'Internal server error' })
+    return res.status(500).json({ error: 'Internal server error' })
   }
 }
 
-export const isUserLoggedIn = async (req: Request, res: Response) => {
+export const sessionsHandler = async (req: Request, res: Response) => {
   try {
-    const sessionTokenFromBrowser = req.cookies.sessionToken
-    if (sessionTokenFromBrowser) {
-      const session = await db.session.findUnique({
-        where: { sessionToken: sessionTokenFromBrowser }
-      })
-      if (session) {
-        const user = await db.user.findUnique({ where: { id: session.userId } })
-        if (user) {
-          return res
-            .status(200)
-            .json({ result: true, messgae: 'user logged in', user })
-        }
-      }
+    console.log(req.cookies, 's')
+    const { sessionId } = req.cookies
+    if (!sessionId) {
+      return res
+        .status(404)
+        .json({ result: false, message: 'User not logged in' })
     }
-  } catch (error) {
-    console.error('Internal Server Error')
-    res.status(500).json({ result: false, message: 'Internal server error' })
+    const session = await db.session.findUnique({
+      where: { sessionToken: sessionId }
+    })
+    if (!session) {
+      return res
+        .status(404)
+        .json({ result: false, message: 'User not logged in' })
+    }
+    const user = await db.user.findUnique({ where: { id: session.userId } })
+    if (!user) {
+      return res
+        .status(404)
+        .json({ result: false, message: 'User not logged in' })
+    }
+    return res
+      .status(200)
+      .json({ result: true, message: 'user logged in', user })
+  } catch (err) {
+    res.status(500).json({ error: `Server Error${err}` })
+  }
+}
+
+export const logoutHandler = async (req: Request, res: Response) => {
+  try {
+    const { sessionId } = req.cookies
+    if (!sessionId) {
+      return res
+        .status(404)
+        .json({ result: false, message: 'user session not found' })
+    }
+    await db.session.delete({
+      where: { sessionToken: sessionId }
+    })
+    res.clearCookie('sessionId')
+    return res.status(200).json({ result: true, message: 'user logged out' })
+  } catch (err) {
+    console.error(err)
+    return res.status(500).json({ error: 'Internal server error' })
   }
 }
